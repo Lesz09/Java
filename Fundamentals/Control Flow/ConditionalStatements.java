19ee9c3a85b245eebd614ba5915370529f7e37ba
lesz <lesz_09@outlook.com>
2026-09-20 13:04:56 -0600

    Added a Java program

---
+++ Fundamentals/Control Flow/ConditionalStatements.java
@@ -0,0 +1,6 @@
+public class ConditionalStatements {
+    public static void main (String[] args) {
+        final int x = 8;
+        if (x <= 19) {System.out.println("It's well");} else if (x == 1) {return;} else {System.out.println("It's worst");} 
+    }
+}
