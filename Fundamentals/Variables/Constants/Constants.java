942e377d258a6941101539e8a73a7f74cc839f83
lesz <lesz_09@outlook.com>
2026-09-20 12:17:20 -0600

    Modified a Java program

--- Fundamentals/Variables/Constants/Constants.java
+++ Fundamentals/Variables/Constants/Constants.java
@@ -1,4 +1,5 @@
 public class Constants {
+    public final double CM_PER_M = 0.01;
     public static void main (String[] args) {
         final int M_PER_CM = 100;
         System.out.println("89 meters in centimeters:\s"+89*100+"cm");
